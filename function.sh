c88c63370e01d1782a94dced4d26abb260596a1a
ManojKumbi <87463693+Manojkumbi@users.noreply.github.com>
2024-12-28 09:54:06 +0530

    Update function.sh

--- function.sh
+++ function.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#Purpose: Function example. Taking Backup of Particular File
-#Version:1.0
-#Created Date: 2024 Sep 21
-#Modified Date:
-#WebSite: https://arkit.co.in
-#Author: Ankam Ravi Kumar
-
 # START #
 function takebackup (){
         if [ -f $1 ]; then
